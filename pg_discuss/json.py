4ed49a900aad723b6a5ffbe6bb88d55446104001
Steffen Prince <steffen@sprin.io>
2015-08-18 17:42:07 -0700

    Fix faulty encoding of datetime to unix timestamp

--- pg_discuss/json.py
+++ pg_discuss/json.py
@@ -1,9 +1,14 @@
 import datetime
 from flask.json import JSONEncoder
 
+def unix_time(dt):
+    epoch = datetime.datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
 class CustomJSONEncoder(JSONEncoder):
 
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
-            return obj.total_seconds()
+            return unix_time(obj)
         return JSONEncoder.default(self, obj)
