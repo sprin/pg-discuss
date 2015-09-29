9dc22fdf81deedecd64335e591d23b9db569bb1a
Steffen Prince <steffen@sprin.io>
2015-09-29 11:36:01 -0700

    Add option to profiler to dump to file

--- blessed_extensions/profiler.py
+++ blessed_extensions/profiler.py
@@ -6,7 +6,11 @@ from pg_discuss import ext
 class ProfilerExt(ext.AppExtBase):
     def init_app(self, app):
         app.config.setdefault('PROFILER_RESTRICTIONS', [20])
+        app.config.setdefault('PROFILER_DIR', None)
         app.config['PROFILE'] = True
         restrictions = app.config['PROFILER_RESTRICTIONS']
-        app.wsgi_app = ProfilerMiddleware(app.wsgi_app,
-                                          restrictions=restrictions)
+        profile_dir = app.config['PROFILER_DIR']
+        app.wsgi_app = ProfilerMiddleware(
+            app.wsgi_app,
+            restrictions=restrictions,
+            profile_dir=profile_dir)
