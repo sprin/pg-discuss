a9ccdd577c7d933dfd78ac928a31153abfffcc3c
Steffen Prince <steffen@sprin.io>
2015-10-04 15:34:58 -0700

    Add justification of JSON encoder strategy pattern

--- pg_discuss/drivers/iso_date_json_encoder.py
+++ pg_discuss/drivers/iso_date_json_encoder.py
@@ -1,4 +1,24 @@
 """Core JSON encoder. Converts DateTime object to ISO 8601 date strings.
+
+Note that subclassing `JSONEncoder` is not recommended by the author of
+`simplejson` since it is possible to have conflicts with attributes on the
+parent class, or depend on attributes on the parent class that may be changed.
+Also, the author suggests subclassing will be faster since `JSONEncoder` can
+perform "caching of instances when certain arguments are not overriden." The
+author's recommendation to implement the strategy pattern for JSON encoders is
+to implement the strategy as a "default" callable that is passed to
+`JSONEncoder` as an argument.
+See: https://github.com/simplejson/simplejson/issues/124
+
+However, subclasses which provide a new `default` method without acessing or
+modifying other `JSONEncoder` internals should not have these drawbacks.
+Timing of this `JSONEncoder` subclass did not show any difference compared to
+providing a "default" callable.
+
+Finally, Flask offers some JSON helpers, and assumes the user
+will set `app.json_encoder` to a `JSONEncoder` subclass if a custom encoder
+is needed. Therefore, pg-discuss will likewise expect subclasses of
+`JSONEncoder` for the value of `DRIVER_JSON_ENCODER`.
 """
 import datetime
 
